4b67831d1551bf0af247f94d6549599d88be3bf1
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-09-15 18:41:16 +0530

    activating cart part

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,35 +1,4 @@
-const products = [
-  {
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87,
-    },
-    priceCents: 1090,
-  },
-  {
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      stars: 4.0,
-      count: 127,
-    },
-    priceCents: 2095,
-  },
-  {
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      stars: 4.5,
-      count: 56,
-    },
-    priceCents: 799,
-  },
-];
-
-
-let productHtml = '';
+let productHtml = "";
 
 products.forEach((product) => {
   productHtml += `
@@ -52,7 +21,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-            $${(product.priceCents /100).toFixed(2)}
+            $${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -77,7 +46,7 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${product.name}">
             Add to Cart
           </button>
         </div>
@@ -86,4 +55,10 @@ products.forEach((product) => {
 
 //console.log(productHtml);
 
-document.getElementById('js-products-grid').innerHTML = productHtml;
+document.getElementById("js-products-grid").innerHTML = productHtml;
+
+document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log("added product!");
+  });
+});
