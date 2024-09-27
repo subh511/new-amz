4b09ed653294bab7c8b525682636ebbbe889777a
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-09-27 19:12:21 +0530

    adding functionality to delete button

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,13 @@
-export const cart = [{
-  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 2
-},{
-  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
-}];
-
+export let cart = [
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2,
+  },
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1,
+  },
+];
 
 export function addToCart(productId) {
   let matchingItem;
@@ -25,4 +27,14 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+}
+
+function removeFromCart(productId) {
+  const newCart = [];
+  cart.forEach((cartItem) => {
+    if(cartItem.productId != productId){
+      newCart.push(cartItem);
+    }
+  });
+  cart = newCart;
 }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -45,7 +45,7 @@ cart.forEach((cartItem) => {
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -102,5 +102,12 @@ cart.forEach((cartItem) => {
 
 //console.log(cartSummaryHtml)
 document.getElementById('order-summary').innerHTML = cartSummaryHtml;
+
+document.querySelectorAll('.js-delete-link').forEach((link)=>{
+link.addEventListener('click',()=>{
+ const productId =  link.dataset.productId;
+ console.log(productId);
+})
+})
 
 //13:19:50
