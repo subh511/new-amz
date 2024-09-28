b9ee0b66a7e1718cf5811c8db03104e1dcd843d1
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-09-28 19:34:29 +0530

    channelize cart with local storage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,7 @@
-export let cart = [
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+ cart = [
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
@@ -8,6 +11,12 @@ export let cart = [
     quantity: 1,
   },
 ];
+}
+
+
+function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart))
+}
 
 export function addToCart(productId) {
   let matchingItem;
@@ -27,6 +36,7 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -37,4 +47,5 @@ export function removeFromCart(productId) {
     }
   });
   cart = newCart;
+  saveToStorage();
 }
